38944426a652ec82dda907d103315a36836370a7
Mark Slater <mark@absolute.live>
2018-01-11 17:29:11 -0800

    Add delete function

---
+++ inc/src/ajax.delete-segment.php
@@ -0,0 +1,23 @@
+<?php
+
+header('Content-type: application/json');
+
+require_once 'dbconfig.php';
+
+
+if ($_POST != "") {
+
+    $segment = new SEGMENTS($DB_con);
+
+    $response = $segment->deleteSegment($_POST['segment_id']);
+
+
+}
+else {
+    $response = array();
+    $response['status'] = 'error'; // could not delete record
+    $response['message'] = '<span class="fas fa-info-circle"></span> &nbsp; Failed to connect';
+}
+
+
+echo json_encode($response);
